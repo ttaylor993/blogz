f0acd2d47cedd7079e91b067e20cf5d891586e24
Tommie Taylor <ttaylor993@aol.com>
2019-05-26 08:08:38 -0500

    Change to main only

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class User(db.Model):
         
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'signup']
+    allowed_routes = ['login', 'signup', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
 
